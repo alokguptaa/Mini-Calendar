c9edaff83b9b697bf1df1e78b8bd0a9e7eb0893f
Alok Gupta <alokguptaa786030@gmail.com>
2025-11-18 18:04:44 +0530

    Fix date display logic and update year retrieval

--- script.js
+++ script.js
@@ -7,9 +7,10 @@ const today = new Date();
 
 const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "November", "December"];
+const allMonths = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
-date.innerHTML = (today.getDate()<10?"10":" ") + today.getDate();
+date.innerHTML = (today.getDate() < 10 ? "0" : " ") + today.getDate();
 day.innerHTML = weekDays[today.getDay()];
-month.innerHTML = allmonths[today.getMonth()];
-year.innerHTML = today.getYear();
+month.innerHTML = allMonths[today.getMonth()];
+
+year.innerHTML = today.getFullYear();
